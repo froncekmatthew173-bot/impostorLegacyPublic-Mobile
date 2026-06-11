7ba2f6e1065e7292f653175873fc6f04415940cc
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2026-06-11 21:46:57 +0300

    update source/mobile/backend/MobileUtil.hx

--- source/mobile/backend/MobileUtil.hx
+++ source/mobile/backend/MobileUtil.hx
@@ -1,16 +1,5 @@
 package mobile.backend;
 
-#if android
-import extension.androidtools.content.Context as AndroidContext;
-import extension.androidtools.widget.Toast as AndroidToast;
-import extension.androidtools.os.Environment as AndroidEnvironment;
-import extension.androidtools.Permissions as AndroidPermissions;
-import extension.androidtools.Settings as AndroidSettings;
-import extension.androidtools.Tools as AndroidTools;
-import extension.androidtools.os.Build.VERSION as AndroidVersion;
-import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
-#end
-
 import lime.system.System as LimeSystem;
 import haxe.io.Path;
 import haxe.Exception;
@@ -79,7 +68,7 @@ class MobileUtil
 	public static function initDirectory():String {
 		var daPath:String = '';
 		if (!FileSystem.exists(getStorageTypePath()))
-			File.saveContent(getStorageTypePath(), ClientPrefs.data.storageType);
+			File.saveContent(getStorageTypePath(), ClientPrefs.storageType);
 
 		var curStorageType:String = File.getContent(getStorageTypePath());
 
@@ -107,7 +96,7 @@ class MobileUtil
 			case 'EXTERNAL_DATA':
 				daPath = "/sdcard/Android/data/com.motorfrog.impostor";
 			default: //technically not needed but here for safety -ArkoseLabs
-				if (daPath == null || daPath == '') daPath = "/sdcard/.ImpostorLegacy";
+				if (daPath == null || daPath == '') daPath = "/sdcard/Android/data/com.motorfrog.impostor/files";
 		}
 		daPath = Path.addTrailingSlash(daPath);
 		currentDirectory = daPath;
