f580115950fe795e432f58431072b24e901d2c69
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2026-06-12 09:08:29 +0300

    update source/mobile/backend/MobileUtil.hx

--- source/mobile/backend/MobileUtil.hx
+++ source/mobile/backend/MobileUtil.hx
@@ -1,5 +1,16 @@
 package mobile.backend;
 
+#if android
+import extension.androidtools.content.Context as AndroidContext;
+import extension.androidtools.widget.Toast as AndroidToast;
+import extension.androidtools.os.Environment as AndroidEnvironment;
+import extension.androidtools.Permissions as AndroidPermissions;
+import extension.androidtools.Settings as AndroidSettings;
+import extension.androidtools.Tools as AndroidTools;
+import extension.androidtools.os.Build.VERSION as AndroidVersion;
+import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
+#end
+
 import lime.system.System as LimeSystem;
 import haxe.io.Path;
 import haxe.Exception;
